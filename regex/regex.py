af9b223f08a6691feaee22f08d516f9f8b4d5782
Nam <tdnam39@gmail.com>
2021-02-28 14:00:21 +1100

    add print regex

--- regex/regex.py
+++ regex/regex.py
@@ -8,7 +8,7 @@ import json
 numeric_pattern = re.compile('^[0-9]+$')
 product_count_text = "381 Products found"
 product_count_int = int(re.match("^[0-9]+", product_count_text).group())
-print(str(product_count_int))
+print("2.1 Solution: " + str(product_count_int))
 
 
 # 2.2 Write the python code that uses a single search pattern to extract the sequence of characters 
@@ -18,7 +18,7 @@ generic_urls = ["https://www.genericdomain.com/abc/def/1290aodwb23-ghi.img", "ht
 
 for url in generic_urls:
     special_sequence = re.search(r"[0-9]+[a-z0-9]+", url)
-    print(special_sequence.group())
+    print("2.2 Solution: " + special_sequence.group())
 
 
 # 2.3 Assume that you have extracted the below string from the 'script' tag of a web page. 
@@ -31,6 +31,7 @@ with open('data.txt', 'r') as file:
     pattern = re.compile(r"products_storage= (.*);")
     product_storage_parsed = pattern.search(product_storage_string_raw)
     product_storage_json = json.loads(product_storage_parsed.group(1))
+    print("2.3 Solution")
     print(product_storage_json)
     print(type(product_storage_json))
 
